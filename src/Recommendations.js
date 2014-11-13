0501bafd154da5e723c24e6eb84970bfa26f0a4a
Craig Munro <craig@craigmunro.net>
2014-11-13 09:56:10 +0000

    Added support for the following request parameters: - user.id - source.placement - user.referrer - user.agent - user.session - rec.visible - app.type

--- src/Recommendations.js
+++ src/Recommendations.js
@@ -11,10 +11,26 @@ var Recommendations = function(args) {
 	self.target = args.target;
 	self.count = args.count || 6;
 	self.lang = args.lang || { title: "From The Web - sponsored links by Taboola", via: "From" };
-	self.thumbnails = args.thumbnails || { width: 190, height: 190 };
+	self.thumbnails = args.thumbnails || { width: 300, height: 300 };
 	self.type = args.type || "desktop";
+	self.session = "init";
+	
+	self.generateID = function()
+	{
+		if(localStorage.getItem("recommendations-id"))
+		{
+			return localStorage.getItem("recommendations-id");
+		}
+		else
+		{
+			id = Math.random().toString(36).substring(2);
+			localStorage.setItem("recommendations-id", id);
+			
+			return id;
+		}
+	}
 
-	self.endpoint = "//api.taboola.com/1.1/json/" + self.pubid + "/recommendations.get?app.type=" + self.type + "&app.apikey=" + self.key +"&rec.count=" + self.count + "&rec.type=mix&user.session=init&source.type=text&source.id=" + self.id + "&source.url=" + encodeURIComponent(self.url) + "&rec.thumbnail.width=" + self.thumbnails.width + "&rec.thumbnail.height=" + self.thumbnails.height + "&rec.callback=?";
+	self.id = self.generateID();
 
 	self.run = function(self)
 	{
@@ -47,7 +63,7 @@ var Recommendations = function(args) {
 		if(self.url && self.target && self.id && self.pubid)
 		{
 			jQuery.getJSON(
-				self.endpoint,
+				"//api.taboola.com/1.1/json/" + self.pubid + "/recommendations.get?app.type=" + self.type + "&app.apikey=" + self.key +"&rec.count=" + self.count + "&rec.type=mix&rec.visible=true&user.id=" + self.id + "&user.session=" + self.session + "&user.referrer=" +  encodeURIComponent(document.referrer) + "&user.agent=" + encodeURIComponent(navigator.userAgent) + "&source.type=text&source.placement=article&source.id=" + self.id + "&source.url=" + encodeURIComponent(self.url) + "&rec.thumbnail.width=" + self.thumbnails.width + "&rec.thumbnail.height=" + self.thumbnails.height + "&rec.callback=?",
 				self.callback
 			);
 		}
@@ -55,6 +71,9 @@ var Recommendations = function(args) {
 	
 	self.callback = function(json)
 	{
+		self.session = json.session;
+		console.log(self.session);
+		
 		if(json.list.length > 0)
 		{
 			// DOM insertion buffer
@@ -88,15 +107,15 @@ var Recommendations = function(args) {
 	self.template = ' \
 			<li> \
 				<div> \
-					<a href="#" onclick="window.location=\'#{url}\';return false;" rel="nofollow" class="cover" style="background-image: url(#{image});"></a> \
+					<a href="#" onclick="window.open(\'#{url}\', \'_blank\');" rel="nofollow" class="cover" style="background-image: url(#{image});"></a> \
 					<h5> \
-						<a href="#" onclick="window.location=\'#{url}\';return false;" rel="nofollow"> \
+						<a href="#" onclick="window.open(\'#{url}\', \'_blank\');" rel="nofollow"> \
 							#{name} \
 						</a> \
 					</h5> \
 					<p> \
 						#{via} \
-						<a href="#" onclick="window.location=\'#{url}\';return false;" rel="nofollow"> \
+						<a href="#" onclick="window.open(\'#{url}\', \'_blank\');" rel="nofollow"> \
 							#{branding} \
 						</a> \
 					</p> \
